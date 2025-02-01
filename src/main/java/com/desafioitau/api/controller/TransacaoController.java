3b7b6c4da055a446f4cbb464054cbe6f66636c7c
Everton Rocha <imevertonrch@gmail.com>
2025-02-01 20:48:56 -0300

    fix: ajustando retorno com corpo vazio

--- src/main/java/com/desafioitau/api/controller/TransacaoController.java
+++ src/main/java/com/desafioitau/api/controller/TransacaoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -32,7 +33,7 @@ public class TransacaoController {
         List<Transacao> transacoes = transacaoService.listarTransacoes();
 
         return transacoes.isEmpty() ?
-                ResponseEntity.noContent().build() :
+                ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ArrayList<>()) :
                 ResponseEntity.ok(transacoes);
     }
 
