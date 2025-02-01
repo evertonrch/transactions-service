86ca1d1af4bf719fbb50923645b2dd2be2b2620c
Everton Rocha <imevertonrch@gmail.com>
2025-02-01 18:15:16 -0300

    refactory: padrão factory method

--- src/main/java/com/desafioitau/api/service/TransacaoService.java
+++ src/main/java/com/desafioitau/api/service/TransacaoService.java
@@ -1,29 +1,48 @@
 package com.desafioitau.api.service;
 
+import com.desafioitau.api.dto.EstatisticaResponse;
+import com.desafioitau.api.dto.TransacaoRequest;
 import com.desafioitau.api.model.Transacao;
-import com.desafioitau.api.repository.TransacaoRepository;
+import com.desafioitau.api.repository.TransacaoRepositoryMock;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Service
 public class TransacaoService {
 
-    private final TransacaoRepository transacaoRepository;
+    private static final int LIMITE_SEGUNDOS = 60;
+    private final TransacaoRepositoryMock repositoryMock;
 
-    public TransacaoService(TransacaoRepository transacaoRepository) {
-        this.transacaoRepository = transacaoRepository;
+    public TransacaoService(TransacaoRepositoryMock repositoryMock) {
+        this.repositoryMock = repositoryMock;
     }
 
-    public void criaTransacao(Transacao transacao) {
-        transacaoRepository.add(transacao);
+    public void criaTransacao(TransacaoRequest request) {
+        Transacao transacao = new Transacao(request.valor(), request.dataHora());
+        repositoryMock.save(transacao);
     }
 
     public List<Transacao> listarTransacoes() {
-        return transacaoRepository.getTransacoes();
+        return repositoryMock.getTransacoes();
     }
 
     public void deletaTransacoes() {
-        transacaoRepository.deletaTransacoes();
+        repositoryMock.deletaTransacoes();
+    }
+
+    public EstatisticaResponse getEstatistica() {
+        var estatistica = repositoryMock.getTransacoes().stream()
+                .filter(this::ocorreuNosUltimos60Segundos)
+                .mapToDouble(transacao -> transacao.valor().doubleValue())
+                .summaryStatistics();
+
+        return EstatisticaResponse.fromEstatisticas(estatistica);
+    }
+
+    private boolean ocorreuNosUltimos60Segundos(Transacao transacao) {
+        return Duration.between(transacao.dataHora(), OffsetDateTime.now()).getSeconds() <= LIMITE_SEGUNDOS;
     }
 }
